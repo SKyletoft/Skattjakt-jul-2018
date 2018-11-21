61ab54a5e9e566f108f627dea9442903b6699093
SKyletoft <45131343+SKyletoft@users.noreply.github.com>
2018-11-21 16:58:01 +0100

    Finally, hopefully have proper touch support and better reloading on devices with bad internet connections (like, somehow, my laptop)

--- WEB2NR/script.js
+++ WEB2NR/script.js
@@ -2,6 +2,8 @@ console.log("Hackerman!\nIf you got this far, you probably know how to look at t
 
 var auto = false;
 var pause = true;
+var start = false;
+var hasStarted = false;
 var revealString = "32x^2 - 2476.667776x = -34283.41115 ";
 var revealed = [];
 for (var i = 0; i < revealString.length; i++) {revealed[i] = false;}
@@ -23,7 +25,7 @@ function drawLoop () {
 	}
 	if (alldone) {pause = false;}
 	document.getElementById("equation").innerHTML = drawString;
-};
+}
 var player = {
 	X: 50,
 	Y: 10,
@@ -58,15 +60,58 @@ var speedEdit = 0.5;
 var bodyRect = document.body.getBoundingClientRect(),
     elemRect = document.getElementById("theCanvas").getBoundingClientRect(),
     offset   = elemRect.top - bodyRect.top;
-	
+
+function reset() {
+	start = false;
+	hasStarted = false;
+	document.getElementById("tutorialH2").innerHTML = "Move the mouse over the canvas or<br />place your finger on the slider to start";
+	document.getElementById("equation").innerHTML = "loading...";
+	for (var i = 0; i < revealString.length; i++) { revealed[i] = false; }
+	player = {
+		X: 50,
+		Y: 10,
+		YVel: 0,
+		YmaxVel: 10,
+		Yaccel: 0.5,
+		height: 50,
+		width: 10,
+		color: "black"
+	};
+	opponent = {
+		X: 450,
+		Y: 10,
+		YVel: 0,
+		YmaxVel: 10,
+		Yaccel: 0.5,
+		height: 50,
+		width: 10,
+		color: "black"
+	};
+	ball = {
+		X: 250,
+		Y: 250,
+		XVel: 5,
+		YVel: 3,
+		radius: 10,
+		color: "black"
+	};
+	ctx.clearRect(0, 0, 500, 500);
+}
+
 document.addEventListener("mousemove", function (e) {
 	//console.log((e.clientX - elemRect.left) + " " + (e.clientY - elemRect.top));
 	player.Y = (e.clientY - elemRect.top);
 });
 
+document.getElementById("theCanvas").addEventListener("mousemove", function (e) {
+	//console.log((e.clientX - elemRect.left) + " " + (e.clientY - elemRect.top));
+	start = true;
+});
+
 document.getElementById("touchcontrols").addEventListener("input", function (e) {
 	//console.log((e.clientX - elemRect.left) + " " + (e.clientY - elemRect.top));
 	//console.log("input!!!!");
+	start = true;
 	player.Y = document.getElementById("touchcontrols").value;
 });
 
@@ -77,101 +122,109 @@ function reveal () {
 		ok = revealed[random];
 		revealed[random] = true;
 		//console.log(revealed);
-	} while (ok)
+	} while (ok);
 }
-function gameLoop () {
-	if (pause) {
-		ball.X += ball.XVel;
-		ball.Y += ball.YVel;
-		opponent.Y += opponent.YVel;
-		if (auto) {
-			player.Y += player.YVel;
-			//player.Y = ball.Y;
+function gameLoop() {
+	if (start) {
+		if (!hasStarted) {
+			hasStarted = true;
+			document.getElementById("tutorialH2").innerHTML = "<br><br>";
 		}
-		
-		//Clipping
-		if (player.Y < 0) {
-			player.Y = 0;
-			player.YVel = 0;
-		}
-		if (opponent.Y < 0) {
-			opponent.Y = 0;
-			opponent.YVel = 0;
-		}
-		if (player.Y > 500) {
-			player.Y = 500;
-			player.YVel = 0;
-		}
-		if (opponent.Y > 500) {
-			opponent.Y = 500;
-			opponent.YVel = 0;
-		}
-		
-		//Scoring
-		if (ball.X < 25) {
-			//opponent point!
-			location.reload(false);
-		} else if (ball.X > 475) {
-			//player point!
-			ball.XVel = -1.01 * Math.abs(ball.XVel);
-			ball = {
-				X: 250,
-				Y: 250,
-				XVel: -5,
-				YVel: 2,
-				radius: 10,
-				color: "black"
-			};
-			reveal();
-		}
-		
-		//Ball physics
-		if (ball.Y < ball.radius || ball.Y > 500 - ball.radius) {
-			ball.YVel *= -1;
-		}
-		if (((((ball.Y - ball.radius < player.Y + (player.height/2)) &&
-			(ball.Y + ball.radius > player.Y - (player.height/2))))) &&
-			((((ball.X - ball.radius < player.X + (player.width/2)) &&
-			(ball.X + ball.radius > player.X - (player.width/2)))))
-			) {
-			ball.XVel = 1.01 * Math.abs(ball.XVel);
-			if (!auto) {
+		if (pause) {
+			ball.X += ball.XVel;
+			ball.Y += ball.YVel;
+			opponent.Y += opponent.YVel;
+			if (auto) {
+				player.Y += player.YVel;
+				//player.Y = ball.Y;
+			}
+
+			//Clipping
+			if (player.Y < 0) {
+				player.Y = 0;
+				player.YVel = 0;
+			}
+			if (opponent.Y < 0) {
+				opponent.Y = 0;
+				opponent.YVel = 0;
+			}
+			if (player.Y > 500) {
+				player.Y = 500;
+				player.YVel = 0;
+			}
+			if (opponent.Y > 500) {
+				opponent.Y = 500;
+				opponent.YVel = 0;
+			}
+
+			//Scoring
+			if (ball.X < 25) {
+				//opponent point!
+				//location.reload(false);
+				reset();
+				return;
+			} else if (ball.X > 475) {
+				//player point!
+				ball.XVel = -1.01 * Math.abs(ball.XVel);
+				ball = {
+					X: 250,
+					Y: 250,
+					XVel: -5,
+					YVel: 2,
+					radius: 10,
+					color: "black"
+				};
 				reveal();
 			}
-		}
-		if (((((ball.Y - ball.radius < opponent.Y + (opponent.height/2)) &&
-			(ball.Y + ball.radius > opponent.Y - (opponent.height/2))))) &&
-			((((ball.X - ball.radius < opponent.X + (opponent.width/2)) &&
-			(ball.X + ball.radius > opponent.X - (opponent.width/2)))))
+
+			//Ball physics
+			if (ball.Y < ball.radius || ball.Y > 500 - ball.radius) {
+				ball.YVel *= -1;
+			}
+			if (((((ball.Y - ball.radius < player.Y + (player.height / 2)) &&
+				(ball.Y + ball.radius > player.Y - (player.height / 2))))) &&
+				((((ball.X - ball.radius < player.X + (player.width / 2)) &&
+					(ball.X + ball.radius > player.X - (player.width / 2)))))
 			) {
-			ball.XVel = -1.01 * Math.abs(ball.XVel);
-			reveal();
-		}
-		
-		//AI
-		if ((ball.Y + ball.radius) < (opponent.Y - (opponent.height / 2))) {
-			//move up (Y-)
-			opponent.YVel = Math.min(opponent.YmaxVel, opponent.YVel - opponent.Yaccel * Math.pow(Math.abs(opponent.Y - ball.Y) / 10, speedEdit));
-		} else if ((ball.Y - ball.radius) > (opponent.Y + (opponent.height / 2))) {
-			//move down (Y+)
-			opponent.YVel = Math.min(opponent.YmaxVel, opponent.YVel + opponent.Yaccel * Math.pow(Math.abs(opponent.Y - ball.Y) / 10, speedEdit));
+				ball.XVel = 1.01 * Math.abs(ball.XVel);
+				if (!auto) {
+					reveal();
+				}
+			}
+			if (((((ball.Y - ball.radius < opponent.Y + (opponent.height / 2)) &&
+				(ball.Y + ball.radius > opponent.Y - (opponent.height / 2))))) &&
+				((((ball.X - ball.radius < opponent.X + (opponent.width / 2)) &&
+					(ball.X + ball.radius > opponent.X - (opponent.width / 2)))))
+			) {
+				ball.XVel = -1.01 * Math.abs(ball.XVel);
+				reveal();
+			}
+
+			//AI
+			if ((ball.Y + ball.radius) < (opponent.Y - (opponent.height / 2))) {
+				//move up (Y-)
+				opponent.YVel = Math.min(opponent.YmaxVel, opponent.YVel - opponent.Yaccel * Math.pow(Math.abs(opponent.Y - ball.Y) / 10, speedEdit));
+			} else if ((ball.Y - ball.radius) > (opponent.Y + (opponent.height / 2))) {
+				//move down (Y+)
+				opponent.YVel = Math.min(opponent.YmaxVel, opponent.YVel + opponent.Yaccel * Math.pow(Math.abs(opponent.Y - ball.Y) / 10, speedEdit));
+			} else {
+				opponent.YVel = opponent.YVel / 1.01;
+			}
+			if ((ball.Y + ball.radius) < (player.Y - (player.height / 2))) {
+				//move up (Y-)
+				player.YVel = Math.min(player.YmaxVel, player.YVel - player.Yaccel * Math.pow(Math.abs(player.Y - ball.Y) / 10, speedEdit));
+			} else if ((ball.Y - ball.radius) > (player.Y + (player.height / 2))) {
+				//move down (Y+)
+				player.YVel = Math.min(player.YmaxVel, player.YVel + player.Yaccel * Math.pow(Math.abs(player.Y - ball.Y) / 10, speedEdit));
+			} else {
+				player.YVel = player.YVel / 1.01;
+			}
+
+			drawLoop();
 		} else {
-			opponent.YVel = opponent.YVel / 1.01;
+			document.getElementById("quickCSS").innerHTML = "#theCanvas {display: none}#touchcontrols {display:none}";
 		}
-		if ((ball.Y + ball.radius) < (player.Y - (player.height / 2))) {
-			//move up (Y-)
-			player.YVel = Math.min(player.YmaxVel, player.YVel - player.Yaccel * Math.pow(Math.abs(player.Y - ball.Y) / 10, speedEdit));
-		} else if ((ball.Y - ball.radius) > (player.Y + (player.height / 2))) {
-			//move down (Y+)
-			player.YVel = Math.min(player.YmaxVel, player.YVel + player.Yaccel * Math.pow(Math.abs(player.Y - ball.Y) / 10, speedEdit));
-		} else {
-			player.YVel = player.YVel / 1.01;
-		}
-		
-		drawLoop();
-	} else {
-		document.getElementById("quickCSS").innerHTML="#theCanvas {display: none}#touchcontrols {display:none}";
 	}
-};
+}
 
 setInterval(gameLoop, 16);
---
+++ WEB6/script.js
@@ -0,0 +1,30 @@
+console.log("hi?");
+
+function go () {
+	var input = document.getElementById("textbox").value;
+	console.log(input);
+	input = input.toLowerCase();
+	switch (input) {
+		case "enkelt första steg":
+		case "enkelt förstasteg":
+		case "enkeltförstasteg":
+		case "enkelt forsta steg":
+		case "enkelt forstasteg":
+		case "enkeltforstasteg":
+			location="../WEB1NR/index.htm";
+			break;
+		case "det börjar lätt":
+		case "det borjar latt":
+			location="../WEB1OR/index.htm";
+			break;
+		case "c":
+			location="../WEB1RU/index.htm";
+			break;
+		case "d":
+			location="../WEB1ST/index.htm";
+			break;
+		default:
+			alert("Fel, tyvärr\nÄr du helt säker att du har skrivit rätt?");
+			break;
+	}
+}
